db7a3a6e6f7cb68abf0a4e97a389938883f179c3
Acharya Silpa <117834398+Silpa-S-Acharya@users.noreply.github.com>
2023-11-19 21:13:08 +0530

    Update Apply.js

--- Apply.js
+++ Apply.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import axios from "axios"
-const Apply = () => {
+function Apply () {
 
   //const history = useNavigate();
   //const [id, idchange] = useState("");
