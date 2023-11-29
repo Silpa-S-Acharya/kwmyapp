a25a477236f6d296d7f338724d96a01ede55f74f
Acharya Silpa <117834398+Silpa-S-Acharya@users.noreply.github.com>
2023-11-30 00:31:25 +0530

    Update Detail.js

--- Mini Project/backend/models/Detail.js
+++ Mini Project/backend/models/Detail.js
@@ -3,9 +3,55 @@ const mongoose = require('mongoose')
 
 const { Schema } = mongoose;
 const AppSchema = new Schema({
-    fname: String,
-    email: String,
-    address: String,
+    fname: {
+        type: String,
+        required: true,
+        trim: true,
+
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    address: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    mobno: {
+        type:String,
+        required:true,
+        trim:true,
+        unique:true,
+        minlength:10,
+        maxlength:13
+    }, 
+    gender:{
+        type:String,
+        required:true
+    },
+    district:{
+        type:String,
+        required:true
+    },
+    dob:{
+        type:Date,
+        required:true,
+        default:Date.now
+    },
+    panchayat:{
+        type:String,
+        required:true
+    },
+    cardno:{
+        type:String,
+        required:true,
+        minlength:14,
+        maxlength:16
+    }
 });
 
 module.exports = mongoose.model("details", AppSchema)
